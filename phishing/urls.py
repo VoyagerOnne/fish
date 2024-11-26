fc0323dcdccf864bc93db8dfcc21152d665141b4
Voyager1 <65264540+VoyagerOnne@users.noreply.github.com>
2024-11-26 19:12:37 +0300

    Delete phishing directory

--- phishing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('phishing_test/<int:test_id>/track/<int:employee_id>/<str:token>/', views.track_action, name='track_action'),
-]
